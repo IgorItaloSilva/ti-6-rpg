1cdfcb5f244b2c9cb12e7201bd0ca94a85793f9a
Felipe Campos <felipecamposap@gmail.com>
2024-09-19 10:47:09 -0300

    improved player movement system

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -10,30 +10,33 @@ public class PlayerMovement : MonoBehaviour
     // Singleton publico do PlayerMovement
     public static PlayerMovement playerMovement;
 
-    [Header("Referências: ")] [SerializeField]
-    private Rigidbody rb;
+    [Header("Referências: ")] 
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private CharacterController cc;
+    [SerializeField] private CapsuleCollider col;
 
     private CinemachineFreeLook cinemachine;
 
     [SerializeField] private PlayerInput playerInput;
     private InputAction moveAction;
     private InputAction jumpAction;
+    private InputAction dodgeAction;
     private InputAction sprintAction;
 
     private Camera mainCam;
     private Animator animator;
 
     [Header("Parametros: ")] [SerializeField]
-    private float moveSpeed = walkMoveSpeed;
+    private float moveSpeed = walkMoveSpeed, 
+        jumpForce = 7500f;
 
     [SerializeField] private LayerMask groundLayers;
 
     private const float turnSmoothTime = 0.02f,
         walkMoveSpeed = 10f,
         sprintMoveSpeed = 15f,
-        dodgeMoveSpeed = 30f,
+        dodgeMoveSpeed = 50f,
         dodgeDuration = 0.2f,
-        jumpForce = 1500f,
         airDrag = 0f,
         groundDrag = 0f;
 
@@ -48,7 +51,9 @@ public class PlayerMovement : MonoBehaviour
         mainCam = Camera.main;
         cinemachine = mainCam.transform.parent.gameObject.GetComponent<CinemachineFreeLook>();
 
-        HandleInput();
+        ActionsWrapper();
+
+        Physics.gravity *= 2;
     }
 
     private void CreateSingleton()
@@ -59,11 +64,12 @@ public class PlayerMovement : MonoBehaviour
             playerMovement = this; // Instanciar PlayerMovement caso não exista
     }
 
-    private void HandleInput()
+    private void ActionsWrapper()
     {
         // Definir input do player:
         moveAction = playerInput.actions.FindAction("Move");
         jumpAction = playerInput.actions.FindAction("Jump");
+        jumpAction = playerInput.actions.FindAction("Dodge");
         sprintAction = playerInput.actions.FindAction("Sprint");
     }
 
@@ -72,29 +78,18 @@ public class PlayerMovement : MonoBehaviour
         // Ler valores de movimento do InputActionReference
         moveInput = moveAction.ReadValue<Vector2>().normalized;
 
-        // Raycast para baixo para checar se o player está no chão
-        isGrounded = Physics.Raycast(transform.position, Vector3.down, 1.05f, groundLayers);
-
-        // Retornar drag para o padrão quando o player cair no chão
-        if (isGrounded)
-        {
-            rb.drag = groundDrag;
-        }
-        else
-        {
-            rb.AddForce(0f, -(jumpForce / 80), 0f);
-        }
-
         // Checar magnitude do input para aplicar movimentação
-        if ((moveInput.magnitude > 0.1 && isGrounded))
+        if ((moveInput.magnitude > 0.1))
             Move();
 
+        // Raycast para baixo para checar se o player está no chão
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, cc.height / 2 + cc.stepOffset + 0.1f, groundLayers);
+        
         // Pular quando o player apertar o botão e estiver no chão
-        if (jumpAction.triggered && isGrounded && moveInput is not { x: 0f, y: 0f })
-            if (combatMode)
-                StartCoroutine(nameof(Dodge));
-            else
+        if(isGrounded)
+            if(jumpAction.triggered)
                 Jump();
+            if()
 
 #if UNITY_EDITOR
         if (Keyboard.current.cKey.wasPressedThisFrame)
@@ -118,24 +113,38 @@ public class PlayerMovement : MonoBehaviour
         var turnOrientation = Mathf.Atan2(moveInput.x, moveInput.y) * Mathf.Rad2Deg + mainCam.transform.eulerAngles.y;
         var smoothedTurnOrientation =
             Mathf.SmoothDampAngle(transform.eulerAngles.y, turnOrientation, ref turnSmoothSpeed, turnSmoothTime);
-        rb.rotation = Quaternion.Euler(0f, smoothedTurnOrientation, 0f);
 
         // Aplicar movimentação multiplicando pela velocidade do player:
         var moveDir = Quaternion.Euler(0f, turnOrientation, 0f) * Vector3.forward;
 
-        // Alternar entre velocidade de corrida e de caminhada
-        var moveVelocity = moveDir * (sprintAction.inProgress ? sprintMoveSpeed : moveSpeed);
-
-        // Aplicar diretamente ao velocity evita que o cinemachine tenha uma convulsão.
-        rb.velocity = new Vector3(moveVelocity.x, rb.velocity.y, moveVelocity.z);
+        // Mover com character controller quando estiver no chão e com o rigidbody quando estiver no ar
+        if (isGrounded)
+        {
+            transform.rotation = Quaternion.Euler(0f, smoothedTurnOrientation, 0f);
+            cc.Move(moveDir * (moveSpeed * Time.deltaTime));
+        }
+        else
+        {
+            rb.AddForce(moveDir * (moveSpeed * 2));
+        }
     }
 
     private void Jump()
     {
         Debug.Log("Jump");
         rb.AddForce(0f, jumpForce, 0f);
-        rb.drag = airDrag; // Diminuir o drag enquanto o player pula para acelerar a queda.
+    }
+
+    private IEnumerator InAir()
+    {
         isGrounded = false;
+        rb.isKinematic = false;
+        rb.velocity = cc.velocity;
+        cc.enabled = false;
+        yield return new WaitUntil(() => isGrounded);
+        cc.enabled = true;
+        rb.isKinematic = true;
+        cc.SimpleMove(rb.velocity);
     }
 
     private IEnumerator Dodge()
