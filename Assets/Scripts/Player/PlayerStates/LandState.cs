9f87856b0d5ea2d918fb6b958839137717ecde0f
Felipe Campos <felipecamposap@gmail.com>
2024-10-10 12:21:19 -0300

    minimized landing effect

--- Assets/Scripts/Player/PlayerStates/LandState.cs
+++ Assets/Scripts/Player/PlayerStates/LandState.cs
@@ -1,6 +1,6 @@
 public class LandState : PlayerState
 {
     public override PlayerMovement.moveStateTypes MoveState => PlayerMovement.moveStateTypes.landing;
-    public override float MoveSpeed => 6f;
-    public override float TurnTime => 0.25f;
+    public override float MoveSpeed => 8f;
+    public override float TurnTime => 0.15f;
 }
