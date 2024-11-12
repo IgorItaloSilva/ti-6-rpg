078a97fd8f1a5e4aadd1894f7add5f9bde6cfd7a
Felipe Campos <felipecamposap@gmail.com>
2024-11-12 10:31:24 -0300

    changed playerinput vectors to passthrough

---
+++ Assets/Scripts/Player/PlayerStates/PlayerGroundedState.cs
@@ -0,0 +1,71 @@
+using System.Diagnostics;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+public class PlayerGroundedState : PlayerBaseState
+{
+
+    private float smoothTime;
+    protected const float MoveSpeed = 8f;
+    public PlayerGroundedState(PlayerStateMachine currentContext, PlayerStateFactory playerStateFactory) : base(
+        currentContext, playerStateFactory)
+    {
+    }
+
+    public override void EnterState()
+    {
+        Debug.Log("Grounded");
+        _ctx.TurnTime = _ctx.BaseTurnTime;
+    }
+
+    public override void UpdateState()
+    {
+        HandleMove();
+        HandleGravity();
+        CheckSwitchStates();
+    }
+
+    public override void ExitState()
+    {
+    }
+
+    public override void CheckSwitchStates()
+    {
+        if (!_ctx.CC.isGrounded)
+        {
+            SwitchState(_factory.InAir());
+            return;
+        }
+        
+        if (_ctx.IsJumpPressed)
+        {
+            HandleJump();
+            SwitchState(_factory.InAir());
+        }
+        
+        if (_ctx.IsSprintPressed)
+        {
+            SwitchState(_factory.Sprint());
+        }
+
+        if (_ctx.IsDodgePressed && _ctx.CanDodge)
+        {
+            SwitchState(_factory.Dodge());
+        }
+    }
+
+    private void HandleMove()
+    {
+        _ctx.AppliedMovement = new Vector3(_ctx.CurrentMovement.x, _ctx.AppliedMovementY, _ctx.CurrentMovement.z);
+        _ctx.CC.Move(_ctx.AppliedMovement * (MoveSpeed * Time.deltaTime));
+    }
+
+    private void HandleGravity()
+    {
+        _ctx.CurrentMovementY = _ctx.BaseGravity;
+    }
+
+    public override void InitializeSubState()
+    {
+    }
+}
