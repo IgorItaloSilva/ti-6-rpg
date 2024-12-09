ee17acf7d722a5753628221eff781cfac1388b7e
IgorItaloSilva <igoritalosilva0@gmail.com>
2024-12-09 13:17:49 -0300

    Contrução level kitsune

---
+++ Assets/Scripts/Kitsune/KitsuneRangeAttack.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+
+public class KitsuneRangeAttack : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    [SerializeField] Transform[] bulletPos;
+    [SerializeField] GameObject rangeBullet;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0;  i < bulletPos.Length; i++)
+        {
+            GameObject clone = Instantiate(rangeBullet, bulletPos[i].position, bulletPos[i].rotation);
+            clone.GetComponent<KitsuneBullet>().SetPlayer(player);
+
+
+        }
+    }
+}
