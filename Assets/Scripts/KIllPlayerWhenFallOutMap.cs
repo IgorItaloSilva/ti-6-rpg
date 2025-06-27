765f8b1a21f3b73cbb43bf773bc87718ced3fd5a
TiagoTozo <tozotiago@gmail.com>
2025-06-27 16:39:48 -0300

    Ajuste death cube e indicador teclas

--- Assets/Scripts/KIllPlayerWhenFallOutMap.cs
+++ Assets/Scripts/KIllPlayerWhenFallOutMap.cs
@@ -2,13 +2,29 @@ using UnityEngine;
 
 public class KIllPlayerWhenFallOutMap : MonoBehaviour
 {
-    void OnTriggerEnter(Collider other){
-        if(other.CompareTag("Player")){
+     void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
             PlayerStats playerStats = other.GetComponent<PlayerStats>();
-            if(playerStats!=null){
-                playerStats.TakeDamage(99999,Enums.DamageType.Regular,false);
+            if (playerStats != null)
+            {
+                playerStats.TakeDamage(99999, Enums.DamageType.Regular, false);
             }
-            
+
+        }
+    } 
+     void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            Debug.Log("o player colidiu com a agua");
+            PlayerStats playerStats = collision.collider.GetComponent<PlayerStats>();
+            if (playerStats != null)
+            {
+                playerStats.TakeDamage(99999, Enums.DamageType.Regular, false);
+            }
+
         }
     }
 }
